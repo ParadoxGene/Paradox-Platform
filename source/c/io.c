3523407e8ba4b98f0bbc22c6ba7d92be573ee195
paradox <109912349+ParadoxGene@users.noreply.github.com>
2024-02-04 23:20:26 -0500

    updated io files

--- source/c/io.c
+++ source/c/io.c
@@ -23,17 +23,21 @@ const errno_t paradox_bin_dir_fopen(
     {
 #if _WIN32
     if(isalpha(filename[0]) && filename[1] == ':') return fopen_s(streamptr, filename, mode);
+    LPSTR program_path;
+    _get_pgmptr(&program_path);
     CHAR program_drive_buf[_MAX_DRIVE];
     CHAR program_dir_buf[_MAX_DIR];
-    _splitpath_s(_pgmptr, program_drive_buf, _MAX_DRIVE, program_dir_buf, _MAX_DIR, NULL, 0, NULL, 0);
-    const DWORD program_dir_buf_sz = strlen(program_dir_buf);
-    const DWORD file_path_buf_sz = strlen(filename);
-    CHAR full_file_path_buf[(_MAX_DRIVE - 1) + program_dir_buf_sz + file_path_buf_sz + 1];
+    _splitpath_s(program_path, program_drive_buf, _MAX_DRIVE, program_dir_buf, _MAX_DIR, NULL, 0, NULL, 0);
+    const size_t program_dir_buf_sz = strlen(program_dir_buf);
+    const size_t file_path_buf_sz = strlen(filename);
+    CHAR* full_file_path_buf = malloc(((_MAX_DRIVE - 1) + program_dir_buf_sz + file_path_buf_sz + 1) * sizeof(CHAR));
     strcpy_s(full_file_path_buf, _MAX_DRIVE, program_drive_buf);
     strcpy_s(full_file_path_buf + (_MAX_DRIVE - 1),  program_dir_buf_sz + 1, program_dir_buf);
     strcpy_s(full_file_path_buf + (_MAX_DRIVE - 1) + program_dir_buf_sz, file_path_buf_sz + 1, filename);
     full_file_path_buf[(_MAX_DRIVE - 1) + program_dir_buf_sz + file_path_buf_sz] = '\0';
-    return fopen_s(streamptr, full_file_path_buf, mode);
+    errno_t err = fopen_s(streamptr, full_file_path_buf, mode);
+    free(full_file_path_buf);
+    return err;
 #endif
     break;
     }
