63c267823480c0604e97115e711938d0d305ff9a
paradox <109912349+ParadoxGene@users.noreply.github.com>
2024-02-04 22:56:16 -0500

    updated io files

--- source/c/io.c
+++ source/c/io.c
@@ -1,6 +1,12 @@
 #include <paradox-platform/io.h>
-#include <stdio.h>
 #include <ctype.h>
+#include <stdio.h>
+
+#if _WIN32
+#include <windows.h>
+#endif
+#include <stdlib.h>
+#include <string.h>
 
 const errno_t paradox_bin_dir_fopen(
     FILE** streamptr,
@@ -17,7 +23,19 @@ const errno_t paradox_bin_dir_fopen(
     {
 #if _WIN32
     if(isalpha(filename[0]) && filename[1] == ':') return fopen_s(streamptr, filename, mode);
+    CHAR program_drive_buf[_MAX_DRIVE];
+    CHAR program_dir_buf[_MAX_DIR];
+    _splitpath_s(_pgmptr, program_drive_buf, _MAX_DRIVE, program_dir_buf, _MAX_DIR, NULL, 0, NULL, 0);
+    const DWORD program_dir_buf_sz = strlen(program_dir_buf);
+    const DWORD file_path_buf_sz = strlen(filename);
+    CHAR full_file_path_buf[(_MAX_DRIVE - 1) + program_dir_buf_sz + file_path_buf_sz + 1];
+    strcpy_s(full_file_path_buf, _MAX_DRIVE, program_drive_buf);
+    strcpy_s(full_file_path_buf + (_MAX_DRIVE - 1),  program_dir_buf_sz + 1, program_dir_buf);
+    strcpy_s(full_file_path_buf + (_MAX_DRIVE - 1) + program_dir_buf_sz, file_path_buf_sz + 1, filename);
+    full_file_path_buf[(_MAX_DRIVE - 1) + program_dir_buf_sz + file_path_buf_sz] = '\0';
+    return fopen_s(streamptr, full_file_path_buf, mode);
 #endif
+    break;
     }
     }
     return 1;
--- tests/tests.c
+++ tests/tests.c
@@ -1,8 +1,11 @@
-#include "paradox-platform/defines.h"
+#include <paradox-platform/defines.h>
+#include <paradox-platform/io.h>
 #include <stdio.h>
 
 int main()
 {
+    FILE* f = NULL;
+    paradox_bin_dir_fopen(&f, "c++_tests.makefile", "r");
     printf("C Test: No errors during compilation.\n");
     printf("C Test: All tests passed.\n");
     return 0;
