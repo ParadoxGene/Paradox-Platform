44b3bf4367e016d5adabbc521e9baf23b100d603
paradox <109912349+ParadoxGene@users.noreply.github.com>
2024-02-07 09:03:35 -0500

    google cmake fix

--- tests/cpp/unit_testing.cpp
+++ tests/cpp/unit_testing.cpp
@@ -1,13 +1,12 @@
+#include <gtest/gtest.h>
+
 #include <paradox-platform/io.hpp>
 #include <iostream>
 
-int main()
-{
-    std::fstream file;
-    Paradox::IO::OpenBinDirFile(file, "cpp_unit_tests");
-    if(file.is_open())
-        printf("File found.\n");
-    std::cout << "C++ Test: No errors during compilation.\n";
-    std::cout << "C++ Test: All tests passed.\n";
-    return 0;
+// Demonstrate some basic assertions.
+TEST(HelloTest, BasicAssertions) {
+  // Expect two strings not to be equal.
+  EXPECT_STRNE("hello", "world");
+  // Expect equality.
+  EXPECT_EQ(7 * 6, 42);
 }
