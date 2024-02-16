0013454bfb9efa7044818625a629d5cb8319c86c
Paradox Gene <109912349+ParadoxGene@users.noreply.github.com>
2024-02-16 12:44:03 -0500

    added a character header

---
+++ api/c/include/paradox-platform/char.h
@@ -0,0 +1,11 @@
+#ifndef PARADOX_WORKS_C_PLATFORM_CHAR_H
+#define PARADOX_WORKS_C_PLATFORM_CHAR_H
+
+#include <paradox-platform/defines.h>
+
+PARADOX_PLATFORM_API const paradox_bool8_t paradox_char8_lower_eq(const paradox_char8_t c1, const paradox_char8_t c2);
+PARADOX_PLATFORM_API const paradox_bool8_t paradox_uchar8_lower_eq(const paradox_uchar8_t c1, const paradox_uchar8_t c2);
+PARADOX_PLATFORM_API const paradox_bool8_t paradox_uchar16_lower_eq(const paradox_uchar16_t c1, const paradox_uchar16_t c2);
+PARADOX_PLATFORM_API const paradox_bool8_t paradox_uchar32_lower_eq(const paradox_uchar32_t c1, const paradox_uchar32_t c2);
+
+#endif
---
+++ api/c/src/char.c
@@ -0,0 +1,21 @@
+#include <paradox-platform/char.h>
+#include <ctype.h>
+
+PARADOX_PLATFORM_API const paradox_bool8_t paradox_char8_lower_eq(const paradox_char8_t c1, const paradox_char8_t c2)
+{
+    return (tolower(c1) == tolower(c2)) ? PARADOX_TRUE : PARADOX_FALSE;
+}
+PARADOX_PLATFORM_API const paradox_bool8_t paradox_uchar8_lower_eq(const paradox_uchar8_t c1, const paradox_uchar8_t c2)
+{
+    return (tolower(c1) == tolower(c2)) ? PARADOX_TRUE : PARADOX_FALSE;
+}
+PARADOX_PLATFORM_API const paradox_bool8_t paradox_uchar16_lower_eq(const paradox_uchar16_t c1, const paradox_uchar16_t c2)
+{
+    return (tolower(c1) == tolower(c2)) ? PARADOX_TRUE : PARADOX_FALSE;
+}
+PARADOX_PLATFORM_API const paradox_bool8_t paradox_uchar32_lower_eq(const paradox_uchar32_t c1, const paradox_uchar32_t c2)
+{
+    if(c1 < sizeof(paradox_char8_t) && c2 < sizeof(paradox_char8_t))
+        return (tolower(c1) == tolower(c2)) ? PARADOX_TRUE : PARADOX_FALSE;
+    else return c1 == c2;
+}
