3e7b1e725b4d5525dd5e3a03293eb8fbcaf95c34
paradox <109912349+ParadoxGene@users.noreply.github.com>
2024-02-17 17:19:46 -0500

    Update char.h

--- api/c/include/paradox-platform/char.h
+++ api/c/include/paradox-platform/char.h
@@ -2,6 +2,7 @@
 #define PARADOX_WORKS_C_PLATFORM_CHAR_H
 
 #include <paradox-platform/defines.h>
+#define PARADOX_UTF8_ERR_CODE (0x10FFFF + 0x1)
 
 PARADOX_PLATFORM_API const paradox_bool8_t paradox_char8_lower_eq(const paradox_char8_t c1, const paradox_char8_t c2);
 PARADOX_PLATFORM_API const paradox_bool8_t paradox_uchar8_lower_eq(const paradox_uchar8_t c1, const paradox_uchar8_t c2);
