28d28e8421a3937d672cb893df719fcdfcc85daa
DeviantCoder <109912349+ParadoxGene@users.noreply.github.com>
2024-07-07 11:25:09 -0400

    Update common.h

--- api/c/paradox-platform/include/paradox-platform/common.h
+++ api/c/paradox-platform/include/paradox-platform/common.h
@@ -1,8 +1,6 @@
 #ifndef PARADOX_SOFTWARE_C_HEADER_PLATFORM_COMMON
 #define PARADOX_SOFTWARE_C_HEADER_PLATFORM_COMMON
 
-#define paradox_offsetof(type, member) ((size_t) &(((type *)0)->member))
-
 typedef enum paradox_os_t
 {
     PARADOX_OS_WINDOWS,
