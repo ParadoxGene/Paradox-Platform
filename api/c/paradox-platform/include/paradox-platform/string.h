c6d96ce0cf3c2c79673d1b3f0b10f7d5d0858c9a
DeviantCoder <109912349+ParadoxGene@users.noreply.github.com>
2024-08-11 20:32:19 -0400

    updated string library

--- api/c/paradox-platform/include/paradox-platform/string.h
+++ api/c/paradox-platform/include/paradox-platform/string.h
@@ -5,27 +5,28 @@
 
 typedef enum paradox_string_errno_t
 {
-    PARADOX_STRING_OK,
+    PARADOX_STRING_SUCCESS,
     PARADOX_STRING_BAD_PTR,
     PARADOX_STRING_BAD_ALLOC,
     PARADOX_STRING_BAD_RANGE,
 } paradox_string_errno_t;
 
 /**
- * Represents a null-terminated string that shrinks/grows by a factor of 2.
+ * Represents a null-terminated string that shrinks/grows by powers of 2.
  */
 typedef struct paradox_string
 {
+    size_t capacity;
+    size_t len;
     paradox_str_t data;
-    size_t data_len;
-    size_t alloc_len;
 } paradox_string;
 
 PARADOX_PLATFORM_API paradox_string* paradox_create_string(paradox_str_t data);
+PARADOX_PLATFORM_API void paradox_clear_string(paradox_string* str);
 PARADOX_PLATFORM_API void paradox_destroy_string(paradox_string* str);
 
-PARADOX_PLATFORM_API paradox_string_errno_t paradox_string_append_str(paradox_string* str, paradox_str_t data);
-PARADOX_PLATFORM_API paradox_string_errno_t paradox_string_append_string(paradox_string* dest, paradox_string* src);
-PARADOX_PLATFORM_API paradox_string_errno_t paradox_string_remove(paradox_string* str, const size_t pos, const size_t len);
+PARADOX_PLATFORM_API paradox_string_errno_t paradox_append_str_to_string(paradox_string* str, paradox_str_t data);
+PARADOX_PLATFORM_API paradox_string_errno_t paradox_append_string_to_string(paradox_string* dest, paradox_string* src);
+PARADOX_PLATFORM_API paradox_string_errno_t paradox_remove_from_string(paradox_string* str, const size_t pos, const size_t len);
 
 #endif
--- api/c/paradox-platform/src/paradox-platform/string.c
+++ api/c/paradox-platform/src/paradox-platform/string.c
@@ -2,77 +2,112 @@
 #include <stdlib.h>
 #include <string.h>
 
-size_t paradox_string_calc_alloc_size(size_t len)
+#define PARADOX_DEFAULT_STRING_CAPACITY 32
+
+size_t paradox_calc_string_alloc_size(const size_t len)
 {
     size_t next_len = 1;
-    while(len >>= 2) next_len <<= 2;
-    return next_len * sizeof(paradox_char8_t);
+    for(size_t i = len; i > 0 ; i >>= 1) next_len <<= 1;
+    if(PARADOX_DEFAULT_STRING_CAPACITY + 1 > next_len) return (PARADOX_DEFAULT_STRING_CAPACITY + 1) * sizeof(paradox_str_char_t);
+    else return (next_len) * sizeof(paradox_str_char_t);
 }
 
 PARADOX_PLATFORM_API paradox_string* paradox_create_string(paradox_str_t data)
 {
     paradox_string* str = malloc(sizeof(paradox_string));
-    str->data_len = strlen(data);
-    if(!str->data_len) str->data = NULL;
+    if(NULL == data)
+    {
+        str->capacity = sizeof(paradox_str_char_t)*(1 + PARADOX_DEFAULT_STRING_CAPACITY);
+        str->data = calloc(str->capacity, sizeof(paradox_str_char_t));
+        str->len = 0;
+    }
     else
     {
-        str->alloc_len = paradox_string_calc_alloc_size(str->data_len + 1);
-        str->data = malloc(str->alloc_len);
-        str->data = memcpy(str->data, data, str->data_len);
-        str->data[str->data_len] = '\0';
+        str->len = strlen(data);
+        if(!str->len)
+        {
+            str->capacity = sizeof(paradox_str_char_t)*(1 + PARADOX_DEFAULT_STRING_CAPACITY);
+            str->data = calloc(str->capacity, sizeof(paradox_str_char_t));
+        }
+        else
+        {
+            str->capacity = paradox_calc_string_alloc_size(str->len + 1);
+            str->data = malloc(str->capacity);
+            str->data = memcpy(str->data, data, str->len);
+            str->data[str->len] = '\0';
+        }
     }
+    
     return str;
 }
-
+PARADOX_PLATFORM_API void paradox_clear_string(paradox_string* str)
+{
+    if(NULL == str) return;
+    if((1 + PARADOX_DEFAULT_STRING_CAPACITY) != str->capacity)
+    {
+        free(str->data);
+        str->capacity = sizeof(paradox_str_char_t)*(1 + PARADOX_DEFAULT_STRING_CAPACITY);
+        str->data = calloc(str->capacity, sizeof(paradox_str_char_t));
+        str->len = 0;
+    }
+    else
+    {
+        if(str->len)
+        {
+            memset(str->data, 0, str->len);
+            str->len = 0;
+        }
+    }
+}
 PARADOX_PLATFORM_API void paradox_destroy_string(paradox_string* str)
 {
     if(NULL == str) return;
-    if(str->data) free(str->data);
+    free(str->data);
     free(str);
 }
 
-PARADOX_PLATFORM_API paradox_string_errno_t paradox_string_append_str(paradox_string* str, paradox_str_t data)
+PARADOX_PLATFORM_API paradox_string_errno_t paradox_append_str_to_string(paradox_string* str, paradox_str_t data)
 {
     if(NULL == str) return PARADOX_STRING_BAD_PTR;
-    size_t data_len = strlen(data);
-    size_t new_alloc_len = paradox_string_calc_alloc_size(str->data_len + data_len + 1);
-    if(new_alloc_len > str->alloc_len)
+    size_t len = strlen(data);
+    size_t new_capacity = paradox_calc_string_alloc_size(str->len + len + 1);
+    if(new_capacity > str->capacity)
     {
-        str->alloc_len = new_alloc_len;
-        str->data = realloc(str->data, str->alloc_len);
+        str->capacity = new_capacity;
+        str->data = realloc(str->data, str->capacity);
     }
-    memcpy(str->data + str->data_len, data, data_len);
-    str->data_len += data_len;
-    str->data[str->data_len] = '\0';
-    return PARADOX_STRING_OK;
+    memcpy(str->data + str->len, data, len);
+    str->len += len;
+    str->data[str->len] = '\0';
+    return PARADOX_STRING_SUCCESS;
 }
 
-PARADOX_PLATFORM_API paradox_string_errno_t paradox_string_append_string(paradox_string* dest, paradox_string* src)
+PARADOX_PLATFORM_API paradox_string_errno_t paradox_append_string_to_string(paradox_string* dest, paradox_string* src)
 {
     if(NULL == dest || NULL == src) return PARADOX_STRING_BAD_PTR;
-    size_t new_alloc_len = paradox_string_calc_alloc_size(dest->data_len + src->data_len + 1);
-    if(new_alloc_len > dest->alloc_len)
+    size_t new_capacity = paradox_calc_string_alloc_size(dest->len + src->len + 1);
+    if(new_capacity > dest->capacity)
     {
-        dest->alloc_len = new_alloc_len;
-        dest->data = realloc(dest->data, dest->alloc_len);
+        dest->capacity = new_capacity;
+        dest->data = realloc(dest->data, dest->capacity);
     }
-    memcpy(dest->data + dest->data_len, src->data, src->data_len);
-    dest->data_len += src->data_len;
-    dest->data[dest->data_len] = '\0';
-    return PARADOX_STRING_OK;
+    memcpy(dest->data + dest->len, src->data, src->len);
+    dest->len += src->len;
+    dest->data[dest->len] = '\0';
+    return PARADOX_STRING_SUCCESS;
 }
 
-PARADOX_PLATFORM_API paradox_string_errno_t paradox_string_remove(paradox_string* str, const size_t pos, const size_t len)
+PARADOX_PLATFORM_API paradox_string_errno_t paradox_remove_from_string(paradox_string* str, const size_t pos, const size_t len)
 {
     if(NULL == str) return PARADOX_STRING_BAD_PTR;
-    if(str->data_len < pos + len) return PARADOX_STRING_BAD_RANGE;
-    memcpy(str->data + pos, str->data + pos + len, (pos + len) - str->data_len + 1);
-    str->data_len -= len;
-    size_t new_alloc_len = paradox_string_calc_alloc_size(str->data_len + 1);
-    if(new_alloc_len > str->alloc_len)
+    if(str->len < pos + len) return PARADOX_STRING_BAD_RANGE;
+    memcpy(str->data + pos, str->data + pos + len, (pos + len) - str->len + 1);
+    str->len -= len;
+    size_t new_capacity = paradox_calc_string_alloc_size(str->len + 1);
+    if(new_capacity > str->capacity)
     {
-        str->alloc_len = new_alloc_len;
-        str->data = realloc(str->data, str->alloc_len);
+        str->capacity = new_capacity;
+        str->data = realloc(str->data, str->capacity);
     }
-    return PARADOX_STRING_OK;
+    return PARADOX_STRING_SUCCESS;
 }
